41d26605e38422b967a7f225d091c8df4ad4c311
Eduardo Francellino <eduardo@aproveita.com.br>
2020-06-15 14:07:43 -0300

    remove disk

--- google-single-vm/main.tf
+++ google-single-vm/main.tf
@@ -34,7 +34,7 @@ variable "gce_ssh_public_key" {
 }
 
 variable "create-extra-disk" {
-  default = "true"
+  default = "false"
 }
 # variable "count" {
 #   default = "${var.create-extra-disk ? 1 : 0}"
