7740979ce1e793ae8ffe6e0470eb60c3fc9b2ab5
Eduardo Francellino <eduardo@aproveita.com.br>
2020-06-15 11:12:50 -0300

    allowing to shutdown to reboot

--- google-single-vm/main.tf
+++ google-single-vm/main.tf
@@ -38,6 +38,7 @@ resource "google_compute_instance" "default" {
   name                      = "${var.unique_resource_name}"
   machine_type              = "${var.machine_type}"
   zone                      = "${var.zone}"
+  allow_stopping_for_update = true
   boot_disk {
     initialize_params {
       image = "${var.boot_disk}"
